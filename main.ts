650c7b6880a565b2e48e0d49eef3918c316dfb13
farhadopal7111 <164672904+farhadopal7111@users.noreply.github.com>
2024-05-13 21:10:39 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+  
+  import inquirer from  "inquirer" 
+ 
+let user_ans =await inquirer.prompt([
+    {
+        name : "words",
+        message : "input",
+        type : "please enter your sentence"
+    }
+])
+
+let word_count = user_ans.words.trim().split("");
+console.log(word_count);
+
+console.log (`your sentence has ${word_count} words.`)
